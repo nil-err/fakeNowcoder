969e966bbb5102f309acee2bd29eca623ff8b2fb
hanyi <yi_han@bupt.edu.cn>
2022-03-08 14:30:48 +0800

    完成邮件发送功能

---
+++ src/main/java/com/han/fakeNowcoder/util/MailClient.java
@@ -0,0 +1,41 @@
+package com.han.fakeNowcoder.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendMail(String receiver, String subject, String content) {
+
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(receiver);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content, true);
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("邮件发送失败" + e.getMessage());
+        }
+
+    }
+
+}
