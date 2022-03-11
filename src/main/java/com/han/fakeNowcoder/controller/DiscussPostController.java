dc51420d48cd2f7bbcbd7e1c8398ba57aa0fb4c2
hanyi <yi_han@bupt.edu.cn>
2022-03-11 11:48:54 +0800

    开发完成帖子详情功能
    使用访问URL的路径变量作为帖子id

--- src/main/java/com/han/fakeNowcoder/controller/DiscussPostController.java
+++ src/main/java/com/han/fakeNowcoder/controller/DiscussPostController.java
@@ -9,6 +9,8 @@ import com.han.fakeNowcoder.util.CommunityUtil;
 import com.han.fakeNowcoder.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,5 +50,16 @@ public class DiscussPostController {
     // 程序出错情况，之后统一处理
 
     return CommunityUtil.getJSONString(0, "发布成功！");
+  }
+
+  @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+  public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+    DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+    model.addAttribute("discussPost", discussPost);
+
+    User user = userService.findUserById(discussPost.getUserId());
+    model.addAttribute("user", user);
+
+    return "/site/discuss-detail";
   }
 }
